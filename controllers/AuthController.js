92845b257ac5cffd536948ef387c1d27bd91e557
rodrigospicker1 <r.spicker13@gmail.com>
2023-03-09 18:46:53 -0300

    register_form_validation

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,3 +1,6 @@
+const User = require('../models/User')
+const bcrypt = require('bcryptjs')
+
 module.exports = class AuthController {
     static login(req, res){
         res.render('auth/login')
@@ -5,5 +8,24 @@ module.exports = class AuthController {
 
     static register(req, res){
         res.render('auth/register')
+    }
+
+    static async registerPost(req, res){
+        const {name, email, password, confirmpassword} = req.body
+
+        //password match
+        if(password != confirmpassword){
+            req.flash('error', 'As senhas são diferentes')
+            res.render('auth/register')
+            return
+        }
+
+        const checkIfUserExists = await User.findOne({where: {email: email}})
+
+        if(checkIfUserExists){
+            req.flash('error', 'O e-mail já está em uso')
+            res.render('auth/register')
+            return
+        }
     }
 }
