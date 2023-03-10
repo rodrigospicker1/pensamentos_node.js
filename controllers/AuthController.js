eca988faa2d4c37f9ebb0fdfb1d29939854d58ee
rodrigospicker1 <r.spicker13@gmail.com>
2023-03-10 08:50:54 -0300

    register_form__validtion

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -10,6 +10,36 @@ module.exports = class AuthController {
         res.render('auth/register')
     }
 
+    static async loginPost(req, res){
+
+        const {email, password} = req.body
+
+        const user = await User.findOne({where: {email: email}})
+        if(!user){
+            req.flash('error', 'Usuário não encontrado')
+            res.render('auth/login')
+
+            return 
+        }
+
+        const passwordMatch = bcrypt.compareSync(password, user.password)
+        if(!passwordMatch){
+            req.flash('error', 'Senha inválida')
+            res.render('auth/login')
+
+            return 
+        }
+
+        req.session.userid = user.id 
+
+        req.flash('success', 'Logado com sucesso!')
+
+        req.session.save(() => {
+            res.redirect('/')
+        })
+
+    }
+
     static async registerPost(req, res){
         const {name, email, password, confirmpassword} = req.body
 
@@ -27,5 +57,36 @@ module.exports = class AuthController {
             res.render('auth/register')
             return
         }
+
+        const salt = bcrypt.genSaltSync(10)
+        const hashedPassword = bcrypt.hashSync(password, salt)
+
+        const user = {
+            name,
+            email,
+            password: hashedPassword
+        }
+
+        try{
+            const createdUser = await User.create(user)
+
+            req.session.userid = createdUser.id 
+            console.log(req.session.userid)
+
+            req.flash('success', 'Cadastro realizado com sucesso!')
+
+            req.session.save(() => {
+                res.redirect('/')
+            })
+
+        }catch(err){
+            console.log(err)
+        }
+
+    }
+
+    static logout(req, res){
+        req.session.destroy()
+        res.redirect('/login')
     }
 }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,6 +5,9 @@ const router = express.Router()
 
 router.get('/login', AuthController.login)
 router.get('/register', AuthController.register)
+
+router.post('/login', AuthController.loginPost)
 router.post('/register', AuthController.registerPost)
+router.post('/logout', AuthController.logout)
 
 module.exports = router
